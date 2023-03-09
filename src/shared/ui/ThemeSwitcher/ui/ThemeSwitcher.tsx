fa13777a44a5b15e8adc06b71ea6f010e849238d
Iv <i.ivanna.liuba@gmail.com>
2023-03-09 13:13:40 +0100

    updated screenshots for tests

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -19,7 +19,7 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className = '' }) => {
       onClick={() => { toggleTheme(); }}
       className={classNames('', {}, [className])}
     >
-      {theme === Themes.DARK ? <ThemeDarkIcon /> : <ThemeLightIcon />}
+      {theme === Themes.DARK ? <ThemeLightIcon /> : <ThemeDarkIcon /> }
     </Button>
   )
 }
